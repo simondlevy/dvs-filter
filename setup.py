6805ce230ad0b0b1fc592d3cea5718e9dfbc962f
simondlevy <simon.d.levy@gmail.com>
2023-08-08 18:45:17 -0400

    dvs-filters => dvs_filters

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ MIT License
 from setuptools import setup
 
 setup (name = 'dvs-filters',
-    version = '0.1',
-    install_requires = ['numpy'],
-    description = 'Python class for DVS filtering',
-    packages = ['dvs_filters'],
-    author='Simon D. Levy',
-    author_email='simon.d.levy@gmail.com',
-    url='https://github.com/simondlevy/eDVS',
-    license='MIT',
-    platforms='Linux; Windows; OS X'
+       version = '0.1',
+       install_requires = ['numpy'],
+       description = 'Python class for DVS filtering',
+       packages = ['dvs_filters'],
+       author='Simon D. Levy',
+       author_email='simon.d.levy@gmail.com',
+       url='https://github.com/simondlevy/dvs-filters',
+       license='MIT',
+       platforms='Linux; Windows; OS X'
     )
